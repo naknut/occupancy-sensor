a2b2c6e85e1fc579b11443ce6c12b640b73daeb0
Marcus Isaksson <me@naknut.email>
2021-04-03 02:05:03 +0100

    Add second sensor

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,12 @@ use crate::sensor::Sensor;
 const POLL_TIME: u64 = 500;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> { 
-    let mut sensor = Sensor::new(Gpio::new()?.get(18)?.into_output(), Gpio::new()?.get(24)?.into_input());
+    let mut outer_sensor = Sensor::new(Gpio::new()?.get(18)?.into_output(), Gpio::new()?.get(24)?.into_input());
+    let mut inner_sensor = Sensor::new(Gpio::new()?.get(17)?.into_output(), Gpio::new()?.get(22)?.into_input());
 
     loop {
-        println!("{}", sensor.is_triggered());
+        println!("Outer: {}", outer_sensor.is_triggered());
+        println!("Inner: {}", inner_sensor.is_triggered());
         thread::sleep(Duration::from_millis(POLL_TIME));
     }
 }
